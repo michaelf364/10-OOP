a9015a4c4ed36414daf915f20187126d7f62566a
Michael Farris <farris.michael.j@gmail.com>
2020-06-10 23:03:02 -0700

    added inquirer prompts

--- app.js
+++ app.js
@@ -10,26 +10,127 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+var engineeringTeam = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+const engineerPrompt = [
+    {
+        type: "input",
+        name: "engineerName",
+        message: "Enter the Engineer's Name.",
+        default: "Bilbo Baggins"
+    },
+    {
+        type: "number",
+        name: "engineerId",
+        message: "Enter the Engineer's ID.",
+        default: 2
+    },
+    {
+        type: "input",
+        name: "engineerEmail",
+        message: "Please enter Engineer's E-mail.",
+        default: "bilbo@baggins.com"
+    },
+    {
+        type: "input",
+        name: "engineerGithub",
+        message: "Enter the Engineer's Github.",
+        default: "BBaggins420"
+    }
+];
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+const internPrompt = [
+    {
+        type: "input",
+        name: "internName",
+        message: "Enter the Intern's Name.",
+        default: "Frodo Baggins"
+    },
+    {
+        type: "number",
+        name: "internId",
+        message: "Enter the Intern's ID.",
+        default: 3
+    },
+    {
+        type: "input",
+        name: "internEmail",
+        message: "Enter the Intern's E-mail.",
+        default: "frodo@baggins.com"
+    },
+    {
+        type: "input",
+        name: "internSchool",
+        message: "Enter the Intern's School.",
+        default: "Shire University"
+    }
+];
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+const managerPrompt = [
+    {
+        type: "input",
+        name: "managerName",
+        message: "Enter the Manager's Name.",
+        default: "Gandalf the Gray"
+    },
+    {
+        type: "number",
+        name: "managerId",
+        message: "Enter the Manager's ID.",
+        default: 1
+    },
+    {
+        type: "input",
+        name: "managerEmail",
+        message: "Enter the Manager's E-mail.",
+        default: "gandalf@gray.com"
+    },
+    {
+        type: "number",
+        name: "officeNumber",
+        message: "Enter the Manager's Office Number.",
+        default: 69
+    }
+];
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+init();
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+function init() {
+    inquirer.prompt(managerPrompt).then(answers => {
+        var newEmployee = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.officeNumber);
+        engineeringTeam.push(newEmployee);
+        teamPrompts();
+    });
+}
+
+function teamPrompts() {
+    inquirer.prompt(rolePrompt).then(answers => {
+        if (answers.employee === "Engineer") {
+            inquirer.prompt(engineerPrompt).then(answers => {
+                var newEmployee = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+                engineeringTeam.push(newEmployee);
+                teamPrompts();
+            });
+        } else if (answers.employee === "Intern") {
+            inquirer.prompt(internPrompt).then(answers => {
+                var newEmployee = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+                engineeringTeam.push(newEmployee);
+                teamPrompts();
+            });
+        } else {
+            var teamHtml = render(engineeringTeam);
+            writeToFile(OUTPUT_DIR, outputPath, teamHtml);
+        }
+    });
+}
+
+function writeToFile(directory, fileName, data) {
+    if (!fs.existsSync(directory)) {
+      fs.mkdirSync(directory);
+          }
+   fs.writeFile(fileName, data, function(err){
+       if(err){
+           return console.log(err);    
+       }
+   });
+}
